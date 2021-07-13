f777ee8d747462e883799fa6a3d8fbfb987b4eb5
ashish <ashish@example.com>
2021-07-14 00:27:51 +0530

    vv

--- app/src/main/java/com/packtpub/my/MainActivity.java
+++ app/src/main/java/com/packtpub/my/MainActivity.java
@@ -8,23 +8,18 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    private GameActivity gameActivity= new   GameActivity();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button playButton= findViewById(R.id.buttonplay);
         playButton.setOnClickListener(this);
-
-
     }
 
     @Override
     public void onClick(View v) {
         Intent i;
-        i= new Intent(this ,gameActivity.getClass());
+        i= new Intent(this ,GameActivity.class);
         startActivity(i);
     }
 }
